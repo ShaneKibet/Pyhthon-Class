06323f465b635ac4e0e09b133bace15639f1b6d5
Shane Kibet <160787579+ShaneKibet@users.noreply.github.com>
2024-02-29 17:17:22 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,16 @@
+
+name= input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+#first approach
+print("Hello ", name, " you are ", age, "year old and live in ", location)
+
+#second approach %
+print("second aproach")
+print(" Hello %s your %d years old and you live %s"%(name, age, location))
+
+#third approach format 
+print("third approach")
+print("Hello {} your {} years old and you live in {}".format(name, age, location))
+ 
